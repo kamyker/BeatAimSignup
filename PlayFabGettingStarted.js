7ec360379cbf9e6d04ae0d9a019160638cd20d59
kamyker <kamil.szurant@gmail.com>
2019-06-02 18:01:40 +0200

    no message

--- PlayFabGettingStarted.js
+++ PlayFabGettingStarted.js
@@ -1,14 +1,12 @@
-
 var titleID = "FB3";
 
 function DoExampleLoginWithCustomID(){
     //PlayFab.settings.titleId = document.getElementById("titleId").value;
     var loginRequest = {
-        // Currently, you need to look up the correct format for this object in the API-docs:
-        // https://api.playfab.com/documentation/Client/method/LoginWithCustomID
         TitleId: titleID,// PlayFab.settings.titleId,
-        CustomId: document.getElementById("customId").value,
-        CreateAccount: true
+        Username: document.getElementById("playerName").value,
+        Email: document.getElementById("email").value,
+		RequireBothUsernameAndEmail: true
     };
 
     PlayFabClientSDK.LoginWithCustomID(loginRequest, LoginCallback);
